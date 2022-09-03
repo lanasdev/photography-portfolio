6d97000bc52efa79627602564fac2540f6937055
DerMatte <me@dermatte.de>
2022-09-03 16:32:29 +0200

    minor footer details

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,14 @@ const Footer = ({ social }) => {
       <div className="px-8 py-8 flex flex-col md:flex-row justify-between items-center">
         <Link href={'/about'}>
           <a>
-            &copy; {new Date().getFullYear() || "2022"} - Bart Photography
+            &copy; {new Date().getFullYear() || "2022"} - <span className="hover:underline">Bart Photography</span>
           </a>
         </Link>
         <div className="flex space-x-6 md:space-x-8 py-4 md:py-0">
-          {social.instagram && (<a href={social?.instagram} target="_blank" rel="noopener noreferrer"><InstagramLogo size={24} /></a>)}
+          {social.instagram && (<a href={social?.instagram} target="_blank" rel="noopener noreferrer" className="hover:rotate-12 ease-in-out duration-200" ><InstagramLogo size={24} /></a>)}
           {/* {console.log("IG", social?.instagram)} */}
-          {social.twitter && (<a href={social?.twitter} target="_blank" rel="noopener noreferrer"><TwitterLogo size={24} /></a>)}
-          {social.email && (<a href={`mailto:${social?.email}`}><Envelope size={24} /></a>)}
+          {social.twitter && (<a href={social?.twitter} target="_blank" rel="noopener noreferrer" className="hover:rotate-12 ease-in-out duration-200" ><TwitterLogo size={24} /></a>)}
+          {social.email && (<a href={`mailto:${social?.email}`} className="hover:rotate-12 ease-in-out duration-200" ><Envelope size={24} /></a>)}
 
         </div>
       </div>
